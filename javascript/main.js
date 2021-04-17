8cb2a64b37efd73293085ed2aed16f7412385eda
Naman Chauhan <chauhannaman98@gmail.com>
2021-04-18 02:06:50 +0530

    AJAX calls & JSON parsing

--- javascript/main.js
+++ javascript/main.js
@@ -7,12 +7,39 @@ button.addEventListener('click', function(){
     pushToDOM(input);
 });
 
+document.querySelector(".js-userinput").addEventListener('keyup', function(e){
+    var input = document.querySelector("input").value;
+    console.log(input);
+
+    // if the key ENTER is pressed ...
+    if(e.which === 13)    {
+        searchGIF(input);
+    }
+});
+
 /* 2. do the data stuff with the API */
 
+function searchGIF(input)   {
+    var url = "https://api.giphy.com/v1/gifs/search?api_key=y9awkI9u1AaJ3uoCQsMKTr88q9zo4o19&q="+input;
 
+    // AJAX request
+    var GiphyAJAXCall = new XMLHttpRequest();
+    GiphyAJAXCall.open( 'GET', url );
+    GiphyAJAXCall.send();
+
+    GiphyAJAXCall.addEventListener('load', function(e){
+        var data = e.target.response;
+        pushToDOM(data);
+    });
+}
 
 /* 3. Show me the GIFs */
 function pushToDOM(input)    {
+    var response = JSON.parse(input);
+
+    var imageURL = response.data[0].images.fixed_height.url;
+    console.log(imageURL);
+
     var container = document.querySelector(".js-container");
-    container.innerHTML = input;
+    container.innerHTML = "<img src=\"" + imageURL + "\">";
 }
